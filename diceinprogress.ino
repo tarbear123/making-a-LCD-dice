69acf846d6002dbc32906c85f7ae6029aca42ddc
tarbear123 <38800609+tarbear123@users.noreply.github.com>
2018-04-28 00:05:38 -0600

    Add files via upload
    i am having trouble getting the select button to roll the dice when it is pressed

---
+++ diceinprogress.ino
@@ -0,0 +1,38 @@
+#include <"DFRkeypad.h">
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+const char* DFRkeypad::sKEY[]=                          { "---",       "Right",   "Up", "Down", "Left", "Select", "???" };
+const int DFRkeypad::iARV_VALUES[DFRkeypad::eNUM_KEYS]= { 1023,        0,         143,  328,    504,     740,      -1   };
+int btnSELECT;
+
+ 
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(1, 0);
+  lcd.print("Roll the dice!");
+}
+
+void loop() {
+while (btnSELECT == 0){
+
+  delay (50);
+  buttonState = analogRead(btnSELECT);
+}
+
+
+ case btnSELECT:
+     {
+     lcd.print("Rolling dice");
+       diceOne = random(1, 7);
+       diceTwo = random(1, 7);
+     break;
+     }
+}
+
+  
+ if((diceOne + diceTwo > 1) && (diceOne + diceTwo <= 7));
+  lcd.print(diceOne + diceTwo);
+ if (diceOne + diceTwo = 7);
+  lcd print ("The Pirate");
+ 
+}
